0b544065e66acc778ae884b0588ef211847bbd30
polrmn <polrmn@gmail.com>
2023-04-27 16:12:45 +0300

    add multer, cloudinary and storage. Updateavatar - done

---
+++ middlewares/upload.js
@@ -0,0 +1,24 @@
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../utils/cloudinary");
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "avatars",
+    allowed_formats: ["jpg", "png", "jpeg", "gif"],
+    transformation: [
+      {
+        width: 250,
+        height: 250,
+        crop: "thumb",
+        gravity: "face",
+      },
+      { quality: "auto:best" },
+    ],
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
