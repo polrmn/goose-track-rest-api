e7b54f3b3487c324bdcdcc6fb4672ddeb23cab7e
polrmn <polrmn@gmail.com>
2023-05-03 15:10:32 +0300

    add avatarURL in currentUser response

--- controllers/auth.js
+++ controllers/auth.js
@@ -68,8 +68,8 @@ const updateData = async (req, res) => {
 };
 
 const current = async (req, res) => {
-  const { name, email, birthday, phone, skype } = req.user;
-  res.json({ name, email, birthday, phone, skype });
+  const { name, email, birthday, phone, skype, avatarURL } = req.user;
+  res.json({ name, email, birthday, phone, skype, avatarURL });
 };
 
 const updateAvatar = async (req, res) => {
